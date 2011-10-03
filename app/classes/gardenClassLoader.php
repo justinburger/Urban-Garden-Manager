03ef4db31c052425eddffb12ef32dc440933b1bf
Justin Burger <j@justinburger.com>
2011-10-02 21:50:03 -0400

    Adding a simple class loader file for the main garden functionality.

---
+++ app/classes/gardenClassLoader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Urban Garden Manager
+ * Garden Class Loader
+ *
+ * Used to load all the required classes for the base garden functionality.
+ *
+ * @author Justin Burger <justin@loudisrelative.com>
+ */
+
+require_once('db.abstract.php');
+require_once('Veggie.class.php');
+require_once('Sqft.class.php');
+require_once('RaisedBed.class.php');
+require_once('garden.class.php');
